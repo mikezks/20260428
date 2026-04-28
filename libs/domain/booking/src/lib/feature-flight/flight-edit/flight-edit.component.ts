101f98ba88236da79aeadad476154de91ff6a51b
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-04-28 13:29:37 +0200

    demo: injection ctx, take until destroyed, effect ctx

--- libs/domain/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
@@ -1,7 +1,13 @@
-import { Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { Component, DestroyRef, Injector, Input, OnChanges, SimpleChanges, inject, runInInjectionContext } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { initialFlight } from '../../logic-flight/model/flight';
+import { timer } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
+
+function injectGreetUser(message: string): void {
+  inject(DestroyRef).onDestroy(() => console.log(message));
+}
 
 @Component({
   selector: 'app-flight-edit',
@@ -13,6 +19,9 @@ import { initialFlight } from '../../logic-flight/model/flight';
 export class FlightEditComponent implements OnChanges {
   @Input() flight = initialFlight;
 
+  private readonly destroyRef = inject(DestroyRef);
+  private readonly injector = inject(Injector);
+
   protected editForm = inject(NonNullableFormBuilder).group({
     id: [0],
     from: [''],
@@ -20,6 +29,14 @@ export class FlightEditComponent implements OnChanges {
     date: [new Date().toISOString()],
     delayed: [false]
   });
+
+  counter = timer(0, 2_000).pipe(
+    takeUntilDestroyed()
+  ).subscribe(console.log);
+
+  constructor() {
+    injectGreetUser('Bye, bye! :(');
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['flight'].previousValue !== changes['flight'].currentValue) {
@@ -29,5 +46,14 @@ export class FlightEditComponent implements OnChanges {
 
   protected save(): void {
     console.log(this.editForm.value);
+
+    /* timer(0, 2_000).pipe(
+      takeUntilDestroyed()
+    ).subscribe(console.log); */
+
+    runInInjectionContext(
+      this.injector,
+      () => injectGreetUser('Bye, bye! :(')
+    );    
   }
 }
