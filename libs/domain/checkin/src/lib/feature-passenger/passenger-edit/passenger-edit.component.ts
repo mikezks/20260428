407d4e7e37853861816b8415ed1b0e6301cdeafe
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-04-29 11:24:08 +0200

    refactor: custom validator for lastname

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,15 +1,34 @@
 import { httpResource } from '@angular/common/http';
 import { Component, input, numberAttribute } from '@angular/core';
-import { form, FormField, required, schema } from '@angular/forms/signals';
+import { form, FormField, required, schema, SchemaPath, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
 
+
+export function validateLastname(
+  field: SchemaPath<string>,
+  allowedLastnames: string[],
+  message: string
+): void {
+  validate(field, ({ value }) =>
+    allowedLastnames.includes(value())
+      ? null
+      : {
+        kind: 'forbiddenLastname',
+        message: message + 'Enter one of the following names: '
+          + allowedLastnames.join(', ')
+      }
+  );
+}
 
 // (3) Field Logic: Validators, conditional disabled, ...
 export const passengerSchema = schema<Passenger>(passengerPath => {
   required(passengerPath.name, {
     message: 'The lastname is mandatory!'
   });
+  validateLastname(passengerPath.name, [
+    'Mustermann', 'Smith'
+  ], 'The lastname is invalid. ');
 });
 
 
